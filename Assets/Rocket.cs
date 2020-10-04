90b8603cce2463bbff45b97395dc2b901739e78a
Daniel Duque <thelupinelunatic@gmail.com>
2020-10-03 17:58:52 -0700

    21_PB_CU2 - Finished first two levels, added level transitions, added death/success sounds, changed thruster sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,69 +1,112 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
 
     Rigidbody rigidBody;
-    AudioSource thrusterSound;
+    AudioSource audioSource;
     
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        thrusterSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondtoThrustInput();
+            RespondtoRotateInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return; // leaves OnCollisionEnter function if state is not alive.
+        }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
+                // do nothing
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                StartSuccessSequence();
                 break;
             default:
-                print("Dead");
-                // kill player if hitting anything else
+                StartDeathSequence();
                 break;
         }
     }
 
-    private void Thrust()
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", 1f); // parameterize time
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+   
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); // Todo: allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondtoThrustInput()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); 
-            if (!thrusterSound.isPlaying) // so the clip doesn't layer and cause sound artifacts
-            {
-                thrusterSound.Play();
-            }
+            ApplyThrust();
         }
         else
         {
-            thrusterSound.Stop();
+            audioSource.Stop();
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying) // so the clip doesn't layer and cause sound artifacts
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondtoRotateInput()
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
-
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * rotationThisFrame);
